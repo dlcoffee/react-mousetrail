8e6ad2b1a75acf2914d1e9f750bc57bb4c846c73
daniel tea <daniel.l.tea@gmail.com>
2021-06-26 09:00:57 -0700

    try this

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-var path = require('path')
-
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-  entry: './index.js',
+  entry: './lib/index.js',
+
+  mode: 'production',
 
   output: {
     path: path.resolve(__dirname, 'build'),
@@ -14,27 +14,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-          },
-        },
-      },
-      {
-        test: /\.*css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-        }),
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
     ],
   },
 
   externals: {
-    react: 'commonjs react',
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
   },
 }
